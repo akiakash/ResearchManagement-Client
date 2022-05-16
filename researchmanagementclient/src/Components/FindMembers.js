7690e8617fb25c1c5dfa2717082f24c1bb2ed3f6
Danushan <it20051648@my.sliit.lk>
2022-05-16 14:21:50 +0530

    done

--- researchmanagementclient/src/Components/FindMembers.js
+++ researchmanagementclient/src/Components/FindMembers.js
@@ -3,47 +3,44 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { Button, Card } from "@mui/material";
 import "@fontsource/roboto";
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-
-
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 14,
-    },
-  }));
-  
-  const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
-    },
-    // hide last border
-    '&:last-child td, &:last-child th': {
-      border: 0,
-    },
-  }));
-  
-  function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-  }
-  
-  const rows = [
-    createData('it20051648', 'dhanu', 'danu@gmail.com', 1562562,),
-    createData('it20051648', 'dhanu', 'danu@gmail.com', 1562562,),
-    createData('it20051648', 'dhanu', 'danu@gmail.com', 1562562,),
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
 
-  ];
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("it20051648", "dhanu", "danu@gmail.com", 1562562),
+  createData("it20051648", "dhanu", "danu@gmail.com", 1562562),
+  createData("it20051648", "dhanu", "danu@gmail.com", 1562562),
+];
 function FindMembers() {
   return (
     <div>
@@ -127,7 +124,7 @@ function FindMembers() {
               // helperText="Incorrect entry."
               style={{ backgroundColor: "white", width: "750px" }}
             />
-             <br></br>
+            <br></br>
             <label>specialization [SE/IT/DS]</label>
             <br></br>
             <TextField
@@ -156,54 +153,53 @@ function FindMembers() {
       <h1 style={{ fontFamily: "Roboto", textAlign: "center" }}>
         <b>Requested Student List</b>
       </h1>
-    
 
       <Card
-      style={{
-        width: "800px",
-        marginRight: "auto",
-        marginLeft: "auto",
-        marginTop: "50px",
-        backgroundColor: "FFF1EF",
-      }}
-    >
-      <Box
-        style={{ marginLeft: "20px", paddingTop: "50px" }}
-        component="form"
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "25ch" },
+        style={{
+          width: "800px",
+          marginRight: "auto",
+          marginLeft: "auto",
+          marginTop: "50px",
+          backgroundColor: "FFF1EF",
         }}
-        noValidate
-        autoComplete="off"
       >
- 
-      <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Student ID </StyledTableCell>
-            <StyledTableCell align="right">Name</StyledTableCell>
-            <StyledTableCell align="right">Email&nbsp;</StyledTableCell>
-            <StyledTableCell align="right">Phone&nbsp;</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {row.name}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    </Box>
-    </Card>
-
+        <Box
+          style={{ marginLeft: "20px", paddingTop: "50px" }}
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell>Student ID </StyledTableCell>
+                  <StyledTableCell align="right">Name</StyledTableCell>
+                  <StyledTableCell align="right">Email&nbsp;</StyledTableCell>
+                  <StyledTableCell align="right">Phone&nbsp;</StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <StyledTableRow key={row.name}>
+                    <StyledTableCell component="th" scope="row">
+                      {row.name}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {row.calories}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                    <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+      </Card>
     </div>
   );
 }
